6390f034b5789c8e95a1bd30887146f95fff2d28
Nicole Baylis <105872694+NicoleBay@users.noreply.github.com>
2024-07-24 17:03:56 +0100

    added deleteSong

--- src/index.js
+++ src/index.js
@@ -170,6 +170,32 @@ const shuffle = () => {
   setPlayButtonAccessibleText();
 };
 
+//delete functionality for the playlist
+//This would manage the removal of a song from the playlist, handle other related actions when a song is deleted, and create a Reset Playlist button
+const deleteSong = (id) => {
+  //check if the song is currently playing. If it is, you need to pause the song and play the next song in the playlist
+  if (userData?.currentSong?.id === id) {
+    userData.currentSong = null;
+    userData.songCurrentTime = 0;
+    pauseSong(); //stop the playback
+    setPlayerDisplay(); //update the player display
+  }
+  userData.songs = userData?.songs.filter((song) => song.id !== id); // Remove the song with the specified id from the userData.songs array
+  //calls:
+  renderSongs(userData?.songs); // displays the modified playlist
+  highlightCurrentSong(); //highlight the current song if there is any
+  setPlayButtonAccessibleText(); //update the play button's accessible text
+  if (userData?.songs.length === 0) {
+    //check if the playlist is empty
+    const resetButton = document.createElement("button"); //create a resetButton element and a text for it
+    const resetText = document.createTextNode("Reset Playlist"); //assign it a text
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText); //added as a child
+    playlistSongs.appendChild(resetButton); //added as a child
+  }
+};
+
 //display the current song title and artist in the player display
 const setPlayerDisplay = () => {
   const playingSong = document.getElementById("player-song-title");
@@ -210,7 +236,7 @@ const renderSongs = (array) => {
 <span class="playlist-song-artist">${song.artist}</span>
 <span class="playlist-song-duration" >${song.duration}</span>
       </button>
-      <button class="playlist-song-delete" aria-label="Delete ${song.title}"><svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg></button>
+      <button class="playlist-song-delete" aria-label="Delete ${song.title}" onclick="deleteSong(${song.id})"><svg width="20" height="20" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="8" fill="#4d4d62"/><path fill-rule="evenodd" clip-rule="evenodd" d="M5.32587 5.18571C5.7107 4.90301 6.28333 4.94814 6.60485 5.28651L8 6.75478L9.39515 5.28651C9.71667 4.94814 10.2893 4.90301 10.6741 5.18571C11.059 5.4684 11.1103 5.97188 10.7888 6.31026L9.1832 7.99999L10.7888 9.68974C11.1103 10.0281 11.059 10.5316 10.6741 10.8143C10.2893 11.097 9.71667 11.0519 9.39515 10.7135L8 9.24521L6.60485 10.7135C6.28333 11.0519 5.7107 11.097 5.32587 10.8143C4.94102 10.5316 4.88969 10.0281 5.21121 9.68974L6.8168 7.99999L5.21122 6.31026C4.8897 5.97188 4.94102 5.4684 5.32587 5.18571Z" fill="white"/></svg></button>
       </li>
       `;
     })
